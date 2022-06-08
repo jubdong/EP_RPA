5c7eb081da00c6a2cd28aa7703ceeea412d526e2
jubdong <106382993+jubdong@users.noreply.github.com>
2022-06-08 11:18:15 +0900

    Update install.py

--- install.py
+++ install.py
@@ -1,2 +1,3 @@
 !pip install jupyter --trusted-host pypi.org --trusted-host files.pythonhosted.org
-!pip install numpy os sys datetime pyautogui clipboard pywinauto selenium==3.141.0 opencv-python pillow openpyxl pandas lxml xlrd bs4 requests html5lib --trusted-host pypi.org --trusted-host files.pythonhosted.org
+!pip install numpy datetime pyautogui clipboard pywinauto selenium==3.141.0 opencv-python pillow openpyxl pandas lxml xlrd bs4 requests html5lib --trusted-host pypi.org --trusted-host files.pythonhosted.org
+# cmd에서 install할 시 !지우고 할 것
