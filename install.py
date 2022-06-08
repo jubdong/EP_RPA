da0861a2493f6629b41ade54422c39be13c706de
jubdong <106382993+jubdong@users.noreply.github.com>
2022-06-08 11:02:15 +0900

    Create install.py

---
+++ install.py
@@ -0,0 +1,1 @@
+!pip3 install numpy os sys datetime pyautogui clipboard pywinauto selenium==3.141.0 opencv-python pillow openpyxl pandas lxml xlrd bs4 requests html5lib --trusted-host pypi.org --trusted-host files.pythonhosted.org
